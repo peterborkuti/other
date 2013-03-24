45c570aba30dfac57a1411688c2f6db6386d47b6
unknown <peter.borkuti@liferay.com>
2013-03-24 20:41:28 +0100

    array - func 0.1

--- JavaScriptBasics/example_functions.js
+++ JavaScriptBasics/example_functions.js
@@ -6,7 +6,7 @@ function say(s) {
 console.log(say("Hello"));
 console.log(say(1));
 console.log(say());
-console.log(say("Hello","Nice");
+console.log(say("Hello","Nice"));
 
 console.log("globals");
 
@@ -27,7 +27,7 @@ function f2_1(){
 	var n = 2;
 	console.log(n);
 }
-f2();
+f2_1();
 console.log(n);
 
 console.log("globals and locals II.");
@@ -39,7 +39,7 @@ function f2_2(){
 	n = 2;
 	console.log(n);
 }
-f3();
+f2_2();
 
 console.log("variables - function scope");
 
@@ -54,3 +54,67 @@ function f3_2(){
 	}
 	console.log(i);
 }
+f3_2();
+
+console.log("function definition, expression")
+
+function f4_1() {
+	return 1;
+};
+
+var f4_2 = function() {
+	return 1;
+};
+
+console.log("immediately invoking");
+
+var f5_1 = function() {
+	return Math.random();
+};
+
+var f5_2 = (function() {
+	return Math.random();
+})();
+
+var f5_3 = (function() {
+	return function() { return Math.random(); };
+})();
+
+console.log(f5_1());
+console.log(f5_1());
+
+//console.log(f5_2());
+console.log(f5_2);
+
+console.log(f5_3());
+console.log(f5_3());
+
+console.log("Nested function");
+
+function f6(s) {
+	function g6() {
+		console.log("g6.s=" + s);
+	}
+	g6();
+};
+
+f6("Hello");
+
+console.log("Object and functions");
+
+var obj = {
+	a : 10,
+	f : function (s) {
+			console.log("obj.f()="+s+","+a);
+		}
+}
+
+obj.f("hello");
+
+function f() {
+	a : 1,
+	f : function() {
+		console.log(a);
+	}
+}
+
