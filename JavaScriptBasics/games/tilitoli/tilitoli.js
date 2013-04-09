a84082c1deb6d544bfbff66faf5a1506c1fa6f70
Peter Borkuti <peter.borkuti@liferay.com>
2013-04-09 09:54:15 +0200

    jslint

--- JavaScriptBasics/games/tilitoli/tilitoli.js
+++ JavaScriptBasics/games/tilitoli/tilitoli.js
@@ -1,48 +1,66 @@
+function initGame() {
+    "use strict";
 
-	function initGame() {
-		var s,i,divs,
-			emptyPos=0,
-			N=4;
-		function getSwapTileWithEmpty(divNum) {
-			return function () { swapTileWithEmpty(divNum) };
-		}
-		function setOnClickXY(x,y){
-			if ((x>=0) && (x<N) && (y>=0) && (y<N)) {
-				var i = y*N+x;
-				divs[i].onclick=getSwapTileWithEmpty(i);
-			}
-		}
-		
-		function swapTileWithEmpty(num) {
-			var imgUrl = divs[num].style.backgroundImage;
-			divs[emptyPos].style.backgroundImage = imgUrl;
-			divs[num].style.backgroundImage = "url(bee-00.png)";
-			emptyPos = num;
-			setOnClicks(num);
-		}
-			
-		function setOnClicks(num) {
-			var i,x,y,x0,x1,y0,y1;
-			for (i=0; i<divs.length; i++) {
-				divs[i].onclick="";
-			}
-			x = num % N;
-			y = (num / N) >> 0;
-			x0 = x-1; x1 = x+1;
-			y0 = y-1; y1 = y+1;
-			setOnClickXY(x0,y);
-			setOnClickXY(x1,y);
-			setOnClickXY(x,y0);
-			setOnClickXY(x,y1);
-		}
-		
-		
-		divs = document.getElementsByTagName('div');
-		for (i=0; i<divs.length; i++) {
-			s = "0".concat(i).slice(-2);
-			divs[i].num = i;
-			divs[i].style.backgroundImage="url(bee-"+s+".png)";
-		}
-		setOnClicks(emptyPos);
-	}
+    var s, i, divs,
+        emptyPos = 0,
+        N = 4;
 
+    function swapTileWithEmpty(num) {
+
+        var imgUrl = divs[num].style.backgroundImage;
+        divs[emptyPos].style.backgroundImage = imgUrl;
+        divs[num].style.backgroundImage = "url(bee-00.png)";
+
+        emptyPos = num;
+        setOnClicks(num);
+
+    }
+
+    function getSwapTileWithEmpty(divNum) {
+
+        return function () { swapTileWithEmpty(divNum); };
+
+    }
+
+    function setOnClickXY(x, y) {
+
+        if ((x >= 0) && (x < N) && (y >= 0) && (y < N)) {
+            var i = y * N + x;
+            divs[i].onclick = getSwapTileWithEmpty(i);
+        }
+
+    }
+
+    function setOnClicks(num) {
+
+        var i, x, y, x0, x1, y0, y1;
+
+        for (i = 0; i < divs.length; i += 1) {
+            divs[i].onclick = "";
+        }
+
+        x = num % N;
+        y = Math.floor(num / N);
+        x0 = x - 1;
+        x1 = x + 1;
+        y0 = y - 1;
+        y1 = y + 1;
+
+        setOnClickXY(x0, y);
+        setOnClickXY(x1, y);
+        setOnClickXY(x, y0);
+        setOnClickXY(x, y1);
+
+    }
+
+    divs = document.getElementsByTagName('div');
+
+    for (i = 0; i < divs.length; i += 1) {
+        s = "0".concat(i).slice(-2);
+        divs[i].num = i;
+        divs[i].style.backgroundImage = "url(bee-" + s + ".png)";
+    }
+
+    setOnClicks(emptyPos);
+
+}
